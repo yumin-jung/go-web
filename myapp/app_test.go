fbaa1444c51cfcef7d81a0aa426c929e11570db9
yumin-jung <jungym887@naver.com>
2022-07-07 17:13:21 +0900

    Implement REST-POST

--- myapp/app_test.go
+++ myapp/app_test.go
@@ -1,9 +1,12 @@
 package myapp
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -37,7 +40,7 @@ func TestUsers(t *testing.T) {
 	assert.Contains(string(data), "Get UserInfo")
 }
 
-func TestUserInfo(t *testing.T) {
+func TestGetUserInfo(t *testing.T) {
 	assert := assert.New(t)
 
 	ts := httptest.NewServer(NewHandler())
@@ -46,7 +49,41 @@ func TestUserInfo(t *testing.T) {
 	resp, err := http.Get(ts.URL + "/users/11")
 	assert.NoError(err)
 	assert.Equal(http.StatusOK, resp.StatusCode)
-
 	data, _ := ioutil.ReadAll(resp.Body)
-	assert.Contains(string(data), "User Id:11")
+	assert.Contains(string(data), "No User Id:11")
+
+	resp, err = http.Get(ts.URL + "/users/12")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ = ioutil.ReadAll(resp.Body)
+	assert.Contains(string(data), "No User Id:12")
+}
+
+func TestCreateUser(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	resp, err := http.Post(ts.URL+"/users", "application/json",
+		strings.NewReader(`{"first_name":"yumin","last_name":"jung","email":"yumin@gmail.com"}`))
+
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user)
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID)
+
+	id := user.ID
+	resp, err = http.Get(ts.URL + "/users/" + strconv.Itoa(id))
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	user2 := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user2)
+	assert.NoError(err)
+	assert.Equal(user.ID, user2.ID)
+	assert.Equal(user.FirstName, user2.FirstName)
 }
